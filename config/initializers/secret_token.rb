805ad814ceace2f5340c96176cd2ca9c4c175c82
Juan Figuera <juanfiguera@gmail.com>
2013-03-27 21:41:01 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codetwits::Application.config.secret_key_base = '612f13be6d8156740f1337c115e81e0de95546d96b5ed753375f9fea61267b753b14ef8e37e65d0d1d3adddec7c4a9d01688f67300a85d2c701601776d31b5f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codetwits::Application.config.session_store :encrypted_cookie_store, key: '_codetwits_session'
